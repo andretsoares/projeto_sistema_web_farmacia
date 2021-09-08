badd7bc8a6cce815733a0ce56d15ed6651c84e6c
andretsoares <andretsoares5@gmail.com>
2021-09-08 11:06:08 -0400

    atualizando icones de barra de tarefas e reescrevendo funcionalidade de troca de botões

---
+++ java-switch.js
@@ -0,0 +1,174 @@
+function selecionar(qualBotao){
+
+          switch (qualBotao) {
+
+              case 'cadastros':
+               
+               document.getElementById("botaoSelecionado1").src = "icone-cadastros2.png"
+			   document.getElementById("botaoSelecionado2").src = "icone-financeiro.png"  	
+			   document.getElementById("botaoSelecionado3").src = "icone-estoque.png"	
+			   document.getElementById("botaoSelecionado4").src = "icone-graficos.png"	
+			   document.getElementById("botaoSelecionado5").src = "icone-relatorios.png"  	
+			   document.getElementById("botaoSelecionado6").src = "icone-vendas.png"	
+			   document.getElementById("botaoSelecionado7").src = "icone-configuracoes.png"	
+			   document.getElementById("botaoSelecionado8").src = "icone-suporte.png"  	
+			   document.getElementById("botaoSelecionado9").src = "icone-ferramentas.png"	
+			   document.getElementById("botaoSelecionado10").src = "icone-sair.png"	
+
+			  
+              break;
+
+              case 'financeiro':
+               
+             
+               document.getElementById("botaoSelecionado1").src = "icone-cadastros.png"
+			   document.getElementById("botaoSelecionado2").src = "icone-financeiro2.png"  	
+			   document.getElementById("botaoSelecionado3").src = "icone-estoque.png"	
+			   document.getElementById("botaoSelecionado4").src = "icone-graficos.png"	
+			   document.getElementById("botaoSelecionado5").src = "icone-relatorios.png"  	
+			   document.getElementById("botaoSelecionado6").src = "icone-vendas.png"	
+			   document.getElementById("botaoSelecionado7").src = "icone-configuracoes.png"	
+			   document.getElementById("botaoSelecionado8").src = "icone-suporte.png"  	
+			   document.getElementById("botaoSelecionado9").src = "icone-ferramentas.png"	
+			   document.getElementById("botaoSelecionado10").src = "icone-sair.png"	
+			  break;
+
+              case 'estoque':
+                
+            
+               document.getElementById("botaoSelecionado1").src = "icone-cadastros.png"
+			   document.getElementById("botaoSelecionado2").src = "icone-financeiro.png"  	
+			   document.getElementById("botaoSelecionado3").src = "icone-estoque2.png"	
+			   document.getElementById("botaoSelecionado4").src = "icone-graficos.png"	
+			   document.getElementById("botaoSelecionado5").src = "icone-relatorios.png"  	
+			   document.getElementById("botaoSelecionado6").src = "icone-vendas.png"	
+			   document.getElementById("botaoSelecionado7").src = "icone-configuracoes.png"	
+			   document.getElementById("botaoSelecionado8").src = "icone-suporte.png"  	
+			   document.getElementById("botaoSelecionado9").src = "icone-ferramentas.png"	
+			   document.getElementById("botaoSelecionado10").src = "icone-sair.png"		
+             
+			 break;
+
+              case 'graficos':
+                
+             
+			   document.getElementById("botaoSelecionado1").src = "icone-cadastros.png"
+			   document.getElementById("botaoSelecionado2").src = "icone-financeiro.png"  	
+			   document.getElementById("botaoSelecionado3").src = "icone-estoque.png"	
+			   document.getElementById("botaoSelecionado4").src = "icone-graficos2.png"	
+			   document.getElementById("botaoSelecionado5").src = "icone-relatorios.png"  	
+			   document.getElementById("botaoSelecionado6").src = "icone-vendas.png"	
+			   document.getElementById("botaoSelecionado7").src = "icone-configuracoes.png"	
+			   document.getElementById("botaoSelecionado8").src = "icone-suporte.png"  	
+			   document.getElementById("botaoSelecionado9").src = "icone-ferramentas.png"	
+			   document.getElementById("botaoSelecionado10").src = "icone-sair.png"	
+             
+			 break;
+			  
+			 case 'relatorios':
+                
+             
+			   document.getElementById("botaoSelecionado1").src = "icone-cadastros.png"
+			   document.getElementById("botaoSelecionado2").src = "icone-financeiro.png"  	
+			   document.getElementById("botaoSelecionado3").src = "icone-estoque.png"	
+			   document.getElementById("botaoSelecionado4").src = "icone-graficos.png"	
+			   document.getElementById("botaoSelecionado5").src = "icone-relatorios2.png"  	
+			   document.getElementById("botaoSelecionado6").src = "icone-vendas.png"	
+			   document.getElementById("botaoSelecionado7").src = "icone-configuracoes.png"	
+			   document.getElementById("botaoSelecionado8").src = "icone-suporte.png"  	
+			   document.getElementById("botaoSelecionado9").src = "icone-ferramentas.png"	
+			   document.getElementById("botaoSelecionado10").src = "icone-sair.png"	
+             
+			 break;
+			 
+			 case 'vendas':
+                
+              
+			   document.getElementById("botaoSelecionado1").src = "icone-cadastros.png"
+			   document.getElementById("botaoSelecionado2").src = "icone-financeiro.png"  	
+			   document.getElementById("botaoSelecionado3").src = "icone-estoque.png"	
+			   document.getElementById("botaoSelecionado4").src = "icone-graficos.png"	
+			   document.getElementById("botaoSelecionado5").src = "icone-relatorios.png"  	
+			   document.getElementById("botaoSelecionado6").src = "icone-vendas2.png"	
+			   document.getElementById("botaoSelecionado7").src = "icone-configuracoes.png"	
+			   document.getElementById("botaoSelecionado8").src = "icone-suporte.png"  	
+			   document.getElementById("botaoSelecionado9").src = "icone-ferramentas.png"	
+			   document.getElementById("botaoSelecionado10").src = "icone-sair.png"	
+             
+			 break;
+			 
+			  case 'configuracoes':
+                
+              
+			   document.getElementById("botaoSelecionado1").src = "icone-cadastros.png"
+			   document.getElementById("botaoSelecionado2").src = "icone-financeiro.png"  	
+			   document.getElementById("botaoSelecionado3").src = "icone-estoque.png"	
+			   document.getElementById("botaoSelecionado4").src = "icone-graficos.png"	
+			   document.getElementById("botaoSelecionado5").src = "icone-relatorios.png"  	
+			   document.getElementById("botaoSelecionado6").src = "icone-vendas.png"	
+			   document.getElementById("botaoSelecionado7").src = "icone-configuracoes2.png"	
+			   document.getElementById("botaoSelecionado8").src = "icone-suporte.png"  	
+			   document.getElementById("botaoSelecionado9").src = "icone-ferramentas.png"	
+			   document.getElementById("botaoSelecionado10").src = "icone-sair.png"	
+             
+			 break;
+			 
+			 case 'suporte':
+                
+             
+			   document.getElementById("botaoSelecionado1").src = "icone-cadastros.png"
+			   document.getElementById("botaoSelecionado2").src = "icone-financeiro.png"  	
+			   document.getElementById("botaoSelecionado3").src = "icone-estoque.png"	
+			   document.getElementById("botaoSelecionado4").src = "icone-graficos.png"	
+			   document.getElementById("botaoSelecionado5").src = "icone-relatorios.png"  	
+			   document.getElementById("botaoSelecionado6").src = "icone-vendas.png"	
+			   document.getElementById("botaoSelecionado7").src = "icone-configuracoes.png"	
+			   document.getElementById("botaoSelecionado8").src = "icone-suporte2.png"  	
+			   document.getElementById("botaoSelecionado9").src = "icone-ferramentas.png"	
+			   document.getElementById("botaoSelecionado10").src = "icone-sair.png"	
+             
+			 break;
+			 
+			  case 'ferramentas':
+                
+            
+			   document.getElementById("botaoSelecionado1").src = "icone-cadastros.png"
+			   document.getElementById("botaoSelecionado2").src = "icone-financeiro.png"  	
+			   document.getElementById("botaoSelecionado3").src = "icone-estoque.png"	
+			   document.getElementById("botaoSelecionado4").src = "icone-graficos.png"	
+			   document.getElementById("botaoSelecionado5").src = "icone-relatorios.png"  	
+			   document.getElementById("botaoSelecionado6").src = "icone-vendas.png"	
+			   document.getElementById("botaoSelecionado7").src = "icone-configuracoes.png"	
+			   document.getElementById("botaoSelecionado8").src = "icone-suporte.png"  	
+			   document.getElementById("botaoSelecionado9").src = "icone-ferramentas2.png"	
+			   document.getElementById("botaoSelecionado10").src = "icone-sair.png"	
+             
+			 break;
+			 
+			 case 'sair':
+                
+           	   document.getElementById("botaoSelecionado1").src = "icone-cadastros.png"
+			   document.getElementById("botaoSelecionado2").src = "icone-financeiro.png"  	
+			   document.getElementById("botaoSelecionado3").src = "icone-estoque.png"	
+			   document.getElementById("botaoSelecionado4").src = "icone-graficos.png"	
+			   document.getElementById("botaoSelecionado5").src = "icone-relatorios.png"  	
+			   document.getElementById("botaoSelecionado6").src = "icone-vendas.png"	
+			   document.getElementById("botaoSelecionado7").src = "icone-configuracoes.png"	
+			   document.getElementById("botaoSelecionado8").src = "icone-suporte.png"  	
+			   document.getElementById("botaoSelecionado9").src = "icone-ferramentas.png"	
+			   document.getElementById("botaoSelecionado10").src = "icone-sair2.png"	
+				  	
+             
+			 break;
+			 
+              default:
+               
+              
+              break;
+
+          }
+
+
+
+
+      }
